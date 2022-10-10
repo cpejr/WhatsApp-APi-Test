03597f0820304583e8825faa6a3431dafc84ebb9
João Pedro Lima Pirajá <jplp100@hotmail.com>
2022-10-10 13:54:50 -0300

    pronto

--- test.js
+++ test.js
@@ -1,15 +1,48 @@
+//-------------------------------------------------------------
+// Contato via API interna
+
 import "dotenv/config";
 import axios from "axios";
 
 const apiTest = axios.create();
+try {
+	const text = "TUDO BEM??";
+	await apiTest.post("https://cpe-whats-api-test.herokuapp.com/send-message", {
+		to: "5571999258225",
+		type: "text",
+		data: {
+			text: { body: text },
+		},
+	});
 
-apiTest.post("https://cpe-whats-api-test.herokuapp.com/send-message", {
-	to: "5571999258225",
-	type: "text",
-	data: {
-		text: { body: "OPAAA" },
-	},
-});
+	const name = "João Pedro Lima Pirajá";
+	await apiTest.post("https://cpe-whats-api-test.herokuapp.com/send-message", {
+		to: "5571999258225",
+		type: "template",
+		data: {
+			template: {
+				name: "doctor_app",
+				language: { code: "pt_BR" },
+				components: [
+					{
+						type: "body",
+						parameters: [
+							{
+								type: "text",
+								text: name,
+							},
+						],
+					},
+				],
+			},
+		},
+	});
+} catch (err) {
+	console.log(err.message);
+}
+
+//--------------------------------------------------------------------------------
+// Contato direto
 
 // import api from "./src/api.js";
 // const senderPhoneId = process.env.PHONE_NUMBER_ID;
