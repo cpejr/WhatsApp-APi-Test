db3285b360f31ec3e827c5d04322cbbfe18e09ad
João Pedro Lima Pirajá <jplp100@hotmail.com>
2022-10-10 12:23:10 -0300

    final version

--- src/server.js
+++ src/server.js
@@ -42,13 +42,13 @@ app.post("/webhook", (req, res, next) => {
 				req.body.entry[0].changes[0].value.messages[0].from
 			);
 			const msg_body = req.body.entry[0].changes[0].value.messages[0].text.body;
-
+			console.log(`Você mandou: ${msg_body}`);
 			api.post(`${phone_number_id}/messages?access_token=${token}`, {
 				messaging_product: "whatsapp",
 				type: "text",
 				to: from,
 				text: {
-					body: { body: `Você mandou: "${msg_body}"` },
+					body: { body: "Você mandou: " + msg_body },
 				},
 			});
 		}
--- src/utils/brazilPhoneFormatter.js
+++ src/utils/brazilPhoneFormatter.js
@@ -3,7 +3,7 @@ const DIGIT_TO_ADD = "9";
 
 export default function brazilPhoneFormatter(phone) {
 	const region = phone.slice(0, 2);
-	if (!(region === BRAZIL_REGION)) return phone;
+	if (region !== BRAZIL_REGION) return phone;
 
 	const formatedPhone = phone.split("");
 	formatedPhone.splice(4, 0, DIGIT_TO_ADD);
